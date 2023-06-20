280b1f95d13a34e6bc8b53678bd6c632fdaf886f
Rene Gomez <39590501+Renegmz@users.noreply.github.com>
2023-06-19 22:00:21 -0400

    [Delete]: Bravo hacks

--- bravo5.py
+++
@@ -1,3 +0,0 @@
-listaA = [100, 200, 300, 400, 500, 600, 700]
-listaB = listaA[:]
-print(listaB)
--- bravo7.py
+++
@@ -1,4 +0,0 @@
-lista = [100, 200, 300, 400, 500, 600, 700]
-cambio = lista.index(300)
-lista[cambio] = "bravo"
-print(lista)
